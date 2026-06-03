dd3fb12715367817154d2cec62f4da6967f71505
linnsheng <linnsheng2002@gmail.com>
2026-06-04 04:54:00 +0800

    Add: sum-of-all-subset-xor-totals - submission-2

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-2.cpp
@@ -0,0 +1,48 @@
+#include <functional>
+#include <vector>
+/*
+ * Find the sum of all the XOR of the subset
+ * Hint: All possible subset --> Either choose or skip
+ * So this is a backtracking problem
+ *
+ * Time Complexity: O(2^n)
+ * Spaec Complexity: O(n)
+ * */
+class Solution
+{
+  public:
+    int subsetXORSum(std::vector<int> &nums)
+    {
+        int ans;
+        std::vector<int> subset;
+
+        std::function<void(std::vector<int> &, const int)> backtracking = [&](std::vector<int> &currSubset,
+                                                                              const int index) {
+            int sum{};
+            for (const auto num : currSubset)
+            {
+                sum ^= num;
+            }
+            ans += sum;
+
+            // Base Case
+            if (index == nums.size())
+            {
+                return;
+            }
+
+            for (int i{index}; i < nums.size(); i++)
+            {
+                // Include it
+                currSubset.emplace_back(nums[i]);
+                backtracking(currSubset, i + 1);
+
+                // Pop it
+                currSubset.pop_back();
+            }
+        };
+
+        backtracking(subset, 0);
+        return ans;
+    }
+};
