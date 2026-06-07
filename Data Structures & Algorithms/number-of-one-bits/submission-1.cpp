3dd2aa19589ff9d5e256cba9480245fd1eaa06ff
linnsheng <linnsheng2002@gmail.com>
2026-06-07 13:58:28 +0800

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.cpp
@@ -0,0 +1,23 @@
+#include <cstdint>
+/*
+ * Find out the number of 1 bits, simply just 'AND' with 1 & keep shifting to the right
+ * */
+class Solution
+{
+  public:
+    int hammingWeight(uint32_t n)
+    {
+        int ans{};
+        while (n)
+        {
+            if (n & 1)
+            {
+                ans++;
+            }
+
+            n = n >> 1;
+        }
+
+        return ans;
+    }
+};
