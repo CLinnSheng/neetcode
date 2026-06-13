1264ed08ba20b9a31bc1f2036b37de12e6c9012e
linnsheng <linnsheng2002@gmail.com>
2026-06-13 11:29:17 +0800

    Add: word-break-ii - submission-3

---
+++ Data Structures & Algorithms/word-break-ii/submission-3.cpp
@@ -0,0 +1,51 @@
+#include <functional>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+/*
+ * Add space in s to construct a sentence where each word is a valid dictionary word
+ * So we are trying to split the string s such that each word is a valid dictionary word
+ * First think to efficeintly check whether is it a valid dicitionary word.
+ * Lets store the wordDict in a hashset instead -> O(1)
+ * We want to find all the possible combination --> Backtracking
+ * At each index we will determine want to split or not by checking is it a valid word
+ *
+ * */
+
+class Solution
+{
+  public:
+    std::vector<std::string> wordBreak(std::string s, std::vector<std::string> &wordDict)
+    {
+        std::unordered_set<std::string> words(wordDict.begin(), wordDict.end());
+        std::vector<std::string> ans;
+        std::string currStr{};
+
+        std::function<void(const int)> backtracking = [&](const int index) {
+            if (index == s.length())
+            {
+                ans.push_back(currStr);
+                return;
+            }
+
+            for (int i{index}; i < s.length(); i++)
+            {
+                std::string substr = s.substr(index, i - index + 1);
+
+                if (words.count(substr))
+                {
+                    auto prev = currStr;
+
+                    currStr += currStr.empty() ? substr : " " + substr;
+                    backtracking(i + 1);
+
+                    //  backtracking
+                    currStr = prev;
+                }
+            }
+        };
+        backtracking(0);
+        return ans;
+    }
+};
