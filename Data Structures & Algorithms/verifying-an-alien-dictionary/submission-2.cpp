b106536164164a0e8405b1a798583c50ba9b99de
linnsheng <linnsheng2002@gmail.com>
2026-06-18 12:02:16 +0800

    Add: verifying-an-alien-dictionary - submission-2

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-2.cpp
@@ -0,0 +1,45 @@
+#include <array>
+#include <string>
+#include <vector>
+/*
+ * Check whether the words are sorted lexicographically according to the alien language
+ *
+ * First thing is we need to build our own dictionary so we know which character come first --> array
+ * */
+class Solution
+{
+  public:
+    bool isAlienSorted(std::vector<std::string> &words, std::string order)
+    {
+        std::array<int, 26> dictionary;
+        for (int i{}; i < 26; i++)
+        {
+            dictionary[order[i] - 'a'] = i;
+        }
+
+        // Check the words
+        for (int i{}; i < words.size() - 1; i++)
+        {
+
+            for (int j{}; j < words[i].length(); j++)
+            {
+                // If same character then the first word must be shorter than the second word
+                if (j == words[i + 1].length())
+                {
+                    return false;
+                }
+
+                if (words[i][j] != words[i + 1][j])
+                {
+                    if (dictionary[words[i + 1][j] - 'a'] < dictionary[words[i][j] - 'a'])
+                    {
+                        return false;
+                    }
+                    // break and check next word
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+};
