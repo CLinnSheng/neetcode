480015db7929bb0709c68e8fab76fe14e0cbdcfb
linnsheng <linnsheng2002@gmail.com>
2026-08-30 16:50:24 +0800

    Add: find-target-in-rotated-sorted-array - submission-16

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-16.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+using std::vector;
+/*
+ * Find the element in the rotated sorted array.
+ * Same make use of the sorted array property so that we can find it in log(n) using binary search
+ * Just need to find which part to search for.
+ * The array is split into 2 increasing parts. Just by comparing the middle element with the last element of the second
+ * part, then we can find out which part we need to search for
+ * */
+class Solution
+{
+  public:
+    int search(vector<int> &nums, int target)
+    {
+        int left{}, right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            // Find the increasing part then check again in the part is it within the range
+            else if (nums[mid] <= nums[right])
+            {
+                if (nums[mid] <= target && nums[right] >= target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            else
+            {
+                if (nums[left] <= target && nums[mid] >= target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
