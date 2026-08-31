97831aacb9dbd66da6cd78bd5bb639ff7140d114
linnsheng <linnsheng2002@gmail.com>
2026-08-31 23:44:26 +0800

    Add: count-good-nodes-in-binary-tree - submission-11

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-11.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+ * Find the number of good nodes within the tree
+ * Good node: If the path from the root of the tree to the node x contains no nodes with a value greater than the value
+ * of node x We can do it in a recursive pattern and pass information about the largest node down so far
+ * */
+class Solution
+{
+  public:
+    int goodNodes(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        return 1 + recursive(root->left, root->val) + recursive(root->right, root->val);
+    }
+
+    int recursive(TreeNode *node, int currMax)
+    {
+        if (node == nullptr)
+        {
+            return 0;
+        }
+
+        int cnt{};
+        if (node->val >= currMax)
+        {
+            currMax = node->val;
+            cnt++;
+        }
+
+        return cnt + recursive(node->left, currMax) +
+               recursive(node->right, currMax);
+    }
+};
