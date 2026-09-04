7e49528d4a274cf7feb141c3f2ef722b0393fc47
linnsheng <linnsheng2002@gmail.com>
2026-09-05 00:13:36 +0800

    Add: permutations - submission-6

---
+++ Data Structures & Algorithms/permutations/submission-6.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using std::vector;
+/*
+ * Time Complexity: O(n! * n)
+ * */
+class Solution
+{
+  public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        vector<int> subset;
+        vector<bool> include(nums.size(), false);
+
+        dfs(nums, subset, ans, include);
+        return ans;
+    }
+
+    void dfs(const vector<int> &nums, vector<int> &subset, vector<vector<int>> &ans, vector<bool> &include)
+    {
+        if (subset.size() == nums.size())
+        {
+            ans.push_back(subset);
+            return;
+        }
+
+        for (int i{}; i < nums.size(); i++)
+        {
+            if (include[i] == false)
+            {
+                include[i] = true;
+                subset.push_back(nums[i]);
+                dfs(nums, subset, ans, include);
+
+                include[i] = false;
+                subset.pop_back();
+            }
+        }
+    }
+};
