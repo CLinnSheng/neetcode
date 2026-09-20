1a2a3905a14702f5bdf29a29fe4a85d8dde57b21
linnsheng <linnsheng2002@gmail.com>
2026-09-20 14:40:53 +0800

    Add: combination-sum-iv - submission-4

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-4.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+       // At each index we can either include it or skip it
+       // From the recursion, we can see some subproblem is solved multiple time
+       // We can cache it and reuse it
+       vector<int> cache(target + 1, -1);
+
+        return dfs(nums,target, 0, cache);
+    }
+
+    int dfs(const std::vector<int>& nums, int target, int currSum, vector<int>& cache)
+    {
+        if (currSum > target)
+        {
+            return 0;
+        }
+
+        if (currSum == target)
+        {
+            return 1;
+        }
+
+        if (cache[currSum] != -1)
+        {
+            return cache[currSum];
+        }
+
+
+        int cnt{};
+        for (int i{}; i < nums.size(); i++)
+        {
+            cnt += dfs(nums, target, currSum + nums[i], cache);
+
+        }
+
+        return cache[currSum] = cnt;
+    }
+};
