00cd6f0bd22ece244e5ab32c90d241d46598a93b
linnsheng <linnsheng2002@gmail.com>
2026-09-21 21:27:13 +0800

    Add: integer-break - submission-6

---
+++ Data Structures & Algorithms/integer-break/submission-6.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cmath>
+#include <vector>
+class Solution
+{
+  public:
+    int integerBreak(int n)
+    {
+        // Break into sum of k positive integers where k  must be at least 2 and maximize the product of those integers
+        // Just recursive break the n and at each point we can either skip or include it
+        // Observation from the recursive approach, some subproblem is solved multiple times
+        // We can cache it and reuse it
+        // cache[i] simply means the maximum product for value i
+
+        std::vector<int> cache(n + 1, -1);
+        return dfs(n, cache);
+    }
+
+    int dfs(int n, std::vector<int>& cache)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        if (cache[n] != -1)
+        {
+            return cache[n];
+        }
+
+        int maxProduct{};
+        for (int i{1}; i < n; i++)
+        {
+            maxProduct = std::max(maxProduct, i * std::max(n - i, dfs(n - i, cache)));
+        }
+
+        return cache[n] = maxProduct;
+    }
+};
